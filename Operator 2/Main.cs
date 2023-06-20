66681fc71b230dbc95b13d331e9fa095a809553f
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-06-20 23:03:54 +0300

    Add project files.

---
+++ Operator 2/Main.cs
@@ -0,0 +1,22 @@
+﻿using Operator_2.Programs;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            GCalculator gCalc = new(2.0, Operation.Addition);
+            gCalc.Calculate();
+
+            CCalculator cCalc = new(3.0, Operation.Subtraction);
+            cCalc.Calculate();
+
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
---
+++ Operator 2/Programs/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Operator_2.Programs
+{
+    internal enum Operation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+    }
+
+    internal abstract class Calculator
+    {
+        protected double Number;
+        protected Operation Op;
+
+        protected Calculator(double number, Operation op)
+        {
+            Number = number;
+            Op = op;
+        }
+
+        public abstract void Calculate();
+    }
+
+    internal class GCalculator : Calculator
+    {
+        public GCalculator(double number, Operation op) : base(number, op)
+        {
+            Console.WriteLine();
+        }
+
+        public override void Calculate()
+        {
+            
+            Console.WriteLine("This is GCalculator. Performing operation: " + Op.ToString());
+        }
+    }
+
+    internal class CCalculator : Calculator
+    {
+        public CCalculator(double number, Operation op) : base(number, op)
+        {
+        }
+
+        public override void Calculate()
+        {
+            // Implement the calculation logic specific to CCalculator
+            Console.WriteLine("This is CCalculator. Performing operation: " + Op.ToString());
+        }
+    }
+
+    
+}
