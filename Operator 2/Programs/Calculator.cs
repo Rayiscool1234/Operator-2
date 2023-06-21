f873d360b9a0fc79d5fcfad7e388354416ea59cb
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-06-22 00:28:50 +0300

    Default app finished

--- Operator 2/Programs/Calculator.cs
+++ Operator 2/Programs/Calculator.cs
@@ -8,17 +8,28 @@ namespace Operator_2.Programs
         Subtraction,
         Multiplication,
         Division,
+        Conversion
+    }
+
+    internal enum Conversion
+    {
+        InchesToFeet,
+        FeetToYards,
+        YardsToMiles
+        // You can add more conversion operations here
     }
 
     internal abstract class Calculator
     {
         protected double[] Numbers;
         protected Operation Op;
+        protected Conversion? Conv;
 
-        protected Calculator(double[] numbers, Operation op)
+        protected Calculator(double[] numbers, Operation op, Conversion? conv = null)
         {
             Numbers = numbers;
             Op = op;
+            Conv = conv;
         }
 
         public abstract void Calculate();
@@ -32,21 +43,62 @@ namespace Operator_2.Programs
 
         public override void Calculate()
         {
-            // Implement the calculation logic specific to GCalculator
-            Console.WriteLine("This is GCalculator. Performing operation: " + Op.ToString());
+            double result = Numbers[0];
+            for (int i = 1; i < Numbers.Length; i++)
+            {
+                switch (Op)
+                {
+                    case Operation.Addition:
+                        result += Numbers[i];
+                        break;
+                    case Operation.Subtraction:
+                        result -= Numbers[i];
+                        break;
+                    case Operation.Multiplication:
+                        result *= Numbers[i];
+                        break;
+                    case Operation.Division:
+                        if (Numbers[i] != 0)
+                            result /= Numbers[i];
+                        else
+                            throw new DivideByZeroException("Cannot divide by zero");
+                        break;
+                }
+            }
         }
     }
 
     internal class CCalculator : Calculator
     {
-        public CCalculator(double[] numbers, Operation op) : base(numbers, op)
+        public CCalculator(double[] numbers, Operation op, Conversion conv) : base(numbers, op, conv)
         {
         }
 
         public override void Calculate()
         {
-            // Implement the calculation logic specific to CCalculator
-            Console.WriteLine("This is CCalculator. Performing operation: " + Op.ToString());
+            if (Op == Operation.Conversion && Conv.HasValue)
+            {
+                double result = Numbers[0]; // Assuming you're converting a single number
+
+                switch (Conv.Value)
+                {
+                    case Conversion.InchesToFeet:
+                        result /= 12;
+                        break;
+                    case Conversion.FeetToYards:
+                        result /= 3;
+                        break;
+                    case Conversion.YardsToMiles:
+                        result /= 1760;
+                        break;
+                }
+
+                Console.WriteLine("CCalculator Conversion Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation for CCalculator");
+            }
         }
     }
 }
