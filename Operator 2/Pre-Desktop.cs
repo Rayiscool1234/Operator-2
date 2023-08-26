c87bc888f42818dd9f6cd0369a753ef0254dfddc
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-08-26 08:02:25 +0300

    Done code

--- Operator 2/Pre-Desktop.cs
+++ Operator 2/Pre-Desktop.cs
@@ -1,23 +1,166 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using CommandLibrary;
 
-
-namespace Operator_2
+// Pre-Desktop
+public class PreDesktop
 {
-    internal class Pre_Desktop
+    public bool Debug = true;
+    public bool Skip = false;
+    public bool ExitNotice = false;
+    public bool ExitRequest = false;
+    public string Version = "v0.0.4 Alpha";
+    public string User;
+    public string Password;
+    public bool systemHasUser = false;
+
+    public void Welcome()
     {
-        static void Main()
+        Console.WriteLine($"Welcome to OperatorOS 2! {Version}");
+        Console.WriteLine("SYSTEM STATEMENT: The user you will create will have Admin permissions");
+        Console.WriteLine("Skip?");
+
+        string output = Console.ReadLine();
+
+        if (output == "y" && Debug)
         {
-            Console.WriteLine("Welcome to OperatorOS 2.0\nWe are currently began starting on preparing your system, Be patient as we setup your system");
-            Thread.Sleep(15000);
-            Console.WriteLine("System pre setup is done.\nCould you please choose an option before booting into the System");
-            Console.WriteLine("Fresh Boot up ~ This boots the system into a fresh enviroment with no users or custom programs !!WARNING: DON'T CHOOSE THIS OPTION IF YOU DON't WANT TO LOSE YOUR USERS OR PROGRAMS!!");
-            Console.WriteLine("Continue current Enviroment ~ OPTIONNOTIMPLEMENTED");
-            string Optionchoicen = Console.ReadLine();
-            Console.WriteLine(Optionchoicen);
+            Skip = true;
+            User = "Tester";
+            Password = "a";
         }
+
+        
+    }
+
+    public bool PromptYesOrNo(string text)
+    {
+        string value;
+
+        do
+        {
+            Console.WriteLine($"{text}\nWould you like to continue with this information?\nWARNING: use Y or N or else you will get an error on this version\nEnter the option below\nOption: ");
+            value = Console.ReadLine().ToLower();
+
+            if (value == "y") return true;
+            if (value == "n") return false;
+
+        } while (value != "y" && value != "n");
+
+        return false;
+    }
+
+    public void Bootup()
+    {
+        Console.WriteLine("OperatorOS 2.0. The new generation of Operations!\n\nYour Operating System is currently preparing. Please standby");
+        Thread.Sleep(15000);
+        Console.WriteLine("Logon menu activated!");
+    }
+
+    public void UserSave()
+    {
+        Console.WriteLine("Saving Credentials\nSaved Credentials");
+
+        // Assuming you have somewhere to save the passwords.
+        // PasswordA = Password;
+        // UserA = User;
+
+        using (StreamWriter UserCredentials = new StreamWriter("UserCredentials.txt"))
+        {
+            UserCredentials.WriteLine(User);
+            UserCredentials.WriteLine(Password);
+        }
+
+
+    }
+
+    public int UserSignUp()
+    {
+        if (Skip) return 0;
+
+        bool yesOrNo;
+
+        do
+        {
+            Console.WriteLine("Please Enter your Username");
+            Console.Write("Username: ");
+            User = Console.ReadLine();
+
+            Console.WriteLine("Please Enter a easy to remember and secure password as you can't change later");
+            Console.Write("Password: ");
+            Password = Console.ReadLine();
+
+            yesOrNo = PromptYesOrNo(User);
+
+        } while (!yesOrNo);
+
+        UserSave();
+
+        return 0;
+    }
+
+    public void UserSignIn()
+    {
+        // TODO: Implement User Sign-in logic
+
+    }
+    public void Logon()
+    {
+        Console.WriteLine("------------ Signing Operation ------------");
+        if (systemHasUser == true) { Console.WriteLine("Sign Up - Creates a new User Profile"); Console.WriteLine("Sign in - Logs in into already existing users"); } else { Console.WriteLine("Sign Up - Creates a new Admin Profile"); Console.WriteLine("Sign in - Unavailable"); }
+        
+    }
+
+    public static void Main(string[] args)
+    {
+        
+
+        PreDesktop preDesktop = new PreDesktop();
+
+        preDesktop.Bootup();
+        preDesktop.Welcome();
+        preDesktop.Logon();
+    }
+}
+
+// Desktop
+public class Desktop
+{
+    public bool ExitNotice = false;
+    public bool ExitRequest = false;
+    public string User;
+
+    public void Commandline()
+    {
+        string command;
+
+        do
+        {
+            Console.Write($"{User}>> ");
+            command = Console.ReadLine();
+
+            if (command == "exit")
+            {
+                ExitNotice = true;
+            }
+            CommandLibrary.CommandLibrary.RunCommand(command);
+            command = "";
+
+        } while (!ExitNotice);
+
+        ExitRequest = true;
+    }
+
+    public void WelcomeToDesktop()
+    {
+        Console.WriteLine($"Welcome, {User}. This Operating System is one of the BEST around for your Operating needs!.");
+        Console.WriteLine("Need to check for help");
+
+        do
+        {
+            Commandline();
+
+        } while (!ExitRequest);
+
+        Environment.Exit(0);
     }
 }
---
+++ Operator 2/Programs/Libcom.cs
@@ -0,0 +1,100 @@
+﻿namespace CommandLibrary
+{
+    public static class CommandLibrary
+    {
+        private static bool scheduledShutdown = false;
+        private static int counter = 0;
+        private static bool highPermission = false;
+        private static bool ranCommand = false;
+
+        private static string passwordA = "NaP";
+        private static string userA;
+        private static string runnableCommand = "Non HR perms:\ncalculator-permissions\nHR perms:\ngivepermissions-logininfo-update";
+        private static string versionA = "V0.0.7 RC";
+        private static string credit = "OperatorOS\nDesigned by Rayan\nCreated by Rayan\nConcept by Rayan\nThank you for using this Applications";
+
+        private static string com = "chelp-help-run-hp-credits-about-test";
+
+        private static bool Find(string input, string searchTerm)
+        {
+            // Perform case-insensitive search
+            var lowercaseInput = input.ToLower();
+            var lowercaseSearchTerm = searchTerm.ToLower();
+
+            // Use String.Contains() method to search for the term
+            return lowercaseInput.Contains(lowercaseSearchTerm);
+        }
+
+        public static void RunCommand(string command)
+        {
+            switch (command)
+            {
+                case "testerror":
+                    // ErrorHandler(); // Call your ErrorHandler function
+                    break;
+                case "test":
+                    Test();
+                    break;
+                case "about":
+                    About();
+                    break;
+                case "hp":
+                    HighPermission();
+                    break;
+                case "run":
+                    Run();
+                    break;
+                case "help":
+                    Help();
+                    break;
+                case "chelp":
+                    CommandHelp();
+                    break;
+                case "credits":
+                    Credits();
+                    break;
+                // Continue with the rest of your commands...
+                default:
+                    Console.WriteLine($"Command \"{command}\" not recognized.");
+                    break;
+            }
+        }
+
+        private static void Test()
+        {
+            Console.WriteLine("Hello, world");
+        }
+
+        private static void About()
+        {
+            Console.WriteLine(versionA);
+        }
+
+        private static void HighPermission()
+        {
+            // Implementation...
+        }
+
+        private static void Run()
+        {
+            // Implementation...
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("QnA\nQ: How do we add applications\nA: You could but you need to have access to the source files which we have open source so add any function that seems fit\nQ: Why do some applications say they are under development\nA: Because these applicatons are planned in the future\nThat's it (if you want to know where to ask these question send them in the Github as a issue)");
+        }
+
+        private static void CommandHelp()
+        {
+            Console.WriteLine($"These are the commands (these dashes are spaces)\n{com}");
+        }
+
+        private static void Credits()
+        {
+            Console.WriteLine(credit);
+        }
+
+        // Continue adding the rest of your functions...
+    }
+}
